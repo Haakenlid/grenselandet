76e3e7bde6776fd3b2b51e568c4383b04eaaa1b5
Håken Lid <haakenlid@gmail.com>
2014-09-07 13:46:53 +0200

    clean up testing django settings file

--- settings/testing.py
+++ settings/testing.py
@@ -1,16 +1,8 @@
-from universitas_no.settings.base import *
-import os
-# TEST SETTINGS
-# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-os.environ['REUSE_DB'] = "1"
-# TEST_RUNNER = 'discover_runner.DiscoverRunner'
-# TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
-# TEST_DISCOVER_ROOT = SITE_ROOT
-# TEST_DISCOVER_PATTERN = "test_*.py"
+""" Settings for running tests """
+from .base import *
+import warnings
 
 # IN-MEMORY TEST DATABASE
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -20,20 +12,12 @@ DATABASES = {
         "HOST": "",
         "PORT": "",
     },
-
-    # 'prodsys': {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": ":memory:",
-    #     "USER": "",
-    #     "PASSWORD": "",
-    #     "HOST": "",
-    #     "PORT": "",
-    # },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # ignore the following error when using ipython:
 #/django/db/backends/sqlite3/base.py:50: RuntimeWarning:
 # SQLite received a naive datetime (2012-11-02 11:20:15.156506) while time zone support is active.
 
-import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning, module='django.db.backends.sqlite3.base', lineno=63)
