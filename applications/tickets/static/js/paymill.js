fa9df891744318016f9a6e148ec5c0f2e5d78ac4
Håken Lid <haakenlid@gmail.com>
2014-09-23 02:28:10 +0200

    feilmeldinger for betalingsskjema

--- applications/tickets/static/js/paymill.js
+++ applications/tickets/static/js/paymill.js
@@ -1,125 +1,117 @@
-// jQuery(document).ready(function ($) {
-
-function logResponse(res) {
-  // create console.log to avoid errors in old IE browsers
-  if (!window.console) console = {log:function(){}};
-  console.log(res);
-
-  if(PAYMILL_TEST_MODE)
-    $('.debug').text(res).show().fadeOut(3000);
-}
-
-  // function PaymillResponseHandler(error, result) {
-  //   if (error) {
-  //     // Shows the error above the form
-  //     $(".payment-errors").text(error.apierror);
-  //     $(".submit-button").removeAttr("disabled");
-  //   } else {
-  //     var form = $("#payment-form");
-  //     // Output token
-  //     var token = result.token;
-  //     // Insert token into form in order to submit to server
-  //     // form.append(token);
-  //     $(".payment-errors").text(token);
-  //   }
-  // }
-       // var doc = document;
-       // var body = $( doc.body );
-
-$('.card-number').keyup(function() {
- var detector = new BrandDetection();
- var brand = detector.detect($('.card-number').val());
- $(".card-number")[0].className = $(".card-number")[0].className.replace(/paymill-card-number-.*/g, '');
- if (brand !== 'unknown') {
-   $('.card-number').addClass("paymill-card-number-" + brand);
-
-   if (!detector.validate($('.card-number').val())) {
-     $('.card-number').addClass("paymill-card-number-grayscale");
-   }
-
-   if (brand !== 'maestro') {
-     VALIDATE_CVC = true;
-   } else {
-     VALIDATE_CVC = false;
-   }
- }
-});
-
-$('.card-expiry').keyup(function() {
- if ( /^\d\d$/.test( $('.card-expiry').val() ) ) {
-   text = $('.card-expiry').val();
-   $('.card-expiry').val(text += "/");
- }
-});
-
-
-function PaymillResponseHandler(error, result) {
- if (error) {
-   $(".payment_errors").css("display","inline-block");
-   $(".payment_errors").text(error.apierror);
- } else {
-   $(".payment_errors").css("display","none");
-   $(".payment_errors").text("");
-   var form = $("#payment-form");
-       // Token
-       var token = result.token;
-       form.append("<input type='hidden' name='paymill_token' value='" + token + "'/>");
-       form.get(0).submit();
-     }
-    $(".submit-button").removeAttr("disabled");
-  }
-
-$("#payment-form").submit(function (event) {
-
-  $('.submit-button').attr("disabled", "disabled");
-
-  if (false === paymill.validateHolder($('.card-holdername').val())) {
-    $(".payment_errors").text("invalid-card-holdername");
-    $(".payment_errors").css("display","inline-block");
-    $(".submit-button").removeAttr("disabled");
-    return false;
-  }
-
-  if ((false === paymill.validateCvc($('.card-cvc').val()))) {
-    if(VALIDATE_CVC){
-      $(".payment_errors").text("invalid-card-cvc");
-      $(".payment_errors").css("display","inline-block");
-      $(".submit-button").removeAttr("disabled");
-      return false;
-    } else {
-      $('.card-cvc').val("000");
+$(function() {
+  $('.card-number').keyup(function() {
+    var detector = new BrandDetection();
+    var brand = detector.detect($('.card-number').val());
+    $(".card-number")[0].className = $(".card-number")[0].className.replace(/paymill-card-number-.*/g, '');
+    if (brand !== 'unknown') {
+      $('.card-number').addClass("paymill-card-number-" + brand);
     }
-  }
+    if (!detector.validate($('.card-number').val())) {
+      $('.card-number').addClass("paymill-card-number-grayscale");
+    }
+    if (brand !== 'maestro') {
+      VALIDATE_CVC = true;
+    } else {
+      VALIDATE_CVC = false;
+    }
+  });
 
-  if (false === paymill.validateCardNumber($('.card-number').val())) {
-    $(".payment_errors").text("invalid-card-number");
-    $(".payment_errors").css("display","inline-block");
-    $(".submit-button").removeAttr("disabled");
+  $('#payment-form').submit(function (event) {
+    $('.submit-button').attr("disabled", "disabled");
+
+    // if (false === paymill.validateHolder($('.card-holdername').val())) {
+    //   $(".payment_errors").text("invalid-card-holdername");
+    //   $(".payment_errors").css("display","inline-block");
+    //   $(".submit-button").removeAttr("disabled");
+    //   return false;
+    // }
+
+    // if (false === paymill.validateCvc($('.card-cvc').val())) {
+    //   if(VALIDATE_CVC){
+    //     $(".payment_errors").text("invalid-card-cvc");
+    //     $(".payment_errors").css("display","inline-block");
+    //     $(".submit-button").removeAttr("disabled");
+    //     return false;
+    //   } else {
+    //     $('.card-cvc').val("000");
+    //   }
+    // }
+
+    // if (false === paymill.validateCardNumber($('.card-number').val())) {
+    //   // $(".payment_errors").text("invalid-card-number");
+    //   $('.card-number').setCustomValidity('invalid card number')
+    //   $(".submit-button").removeAttr("disabled");
+    //   return false;
+    // }
+
+    // var expiry_month = $('.card-expiry-month').val();
+    // var expiry_year = $('.card-expiry-year').val();
+    // if (expiry_year && (exp_year.length <= 2)){
+    //   expiry_year = '20' + expiry_year;
+    // }
+    // if (false === paymill.validateExpiry(expiry_month, expiry_year)) {
+    //   $(".payment_errors").text("invalid-card-expiry-date");
+    //   $(".payment_errors").css("display","inline-block");
+    //   $(".submit-button").removeAttr("disabled");
+    //   return false;
+    // }
+
+    var params = {
+      amount_int:     parseInt($('.card-amount').val().replace(/[\.,]/, '.') * 100),  // E.g. "15" for 0.15 Eur
+      currency:       $('.card-currency').val(),    // ISO 4217 e.g. "EUR"
+      number:         $('.card-number').val(),
+      exp_month:      $('.card-expiry-month').val(),
+      exp_year:       $('.card-expiry-year').val(),
+      cvc:            $('.card-cvc').val(),
+      cardholder:     $('.card-holdername').val()
+    };
+
+    paymill.createToken(params, PaymillResponseHandler);
     return false;
+  });
+
+  function PaymillResponseHandler(error, result) {
+    if (error) {
+      switch (error.apierror){
+        case 'field_invalid_card_number':
+          InputError('.card-number', 'This card number seems to be invalid.');
+          break;
+
+        case 'field_invalid_card_exp':
+          InputError('.card-expiry-month', 'This expiration date or year seems to be invalid.');
+          break;
+
+        case 'field_invalid_card_cvc':
+          InputError('.card-cvc', 'This verficiation code seems to be invalid.');
+          break;
+
+        case 'field_invalid_card_holder':
+          InputError('.card-holdername', 'The name of the credit card holder seems to be invalid.');
+          break;
+
+        default:
+          alert('error in credit card details.')
+          break;
+      }
+    } else {
+      var form = $("#payment-form");
+      var token = result.token;
+      form.append("<input type='hidden' name='paymill_token' value='" + token + "'/>");
+      form.get(0).submit();
+    }
+    $(".submit-button").removeAttr("disabled");
   }
 
-   // var expiry = $('.card-expiry').val();
-   // expiry = expiry.split("/");
-   // if(expiry[1] && (expiry[1].length <= 2)){
-   //   expiry[1] = '20'+expiry[1];
-   // }
-   // if (false === paymill.validateExpiry(expiry[0], expiry[1])) {
-   //   $(".payment_errors").text("invalid-card-expiry-date");
-   //   $(".payment_errors").css("display","inline-block");
-   //   $(".submit-button").removeAttr("disabled");
-   //   return false;
-   // }
+  function InputError(inputclass, message) {
+    input = $(inputclass)[0];
+    input.setCustomValidity(message);
+    input.oninput = function(){
+      this.setCustomValidity('')
+    }
+    $(".submit-button").removeAttr("disabled");
+    document.getElementById("payment-button").click()
 
-  var params = {
-    amount_int:     parseInt($('.card-amount').val().replace(/[\.,]/, '.') * 100),  // E.g. "15" for 0.15 Eur
-    currency:       $('.card-currency').val(),    // ISO 4217 e.g. "EUR"
-    number:         $('.card-number').val(),
-    exp_month:      $('.card-expiry-month').val(),
-    exp_year:       $('.card-expiry-year').val(),
-    cvc:            $('.card-cvc').val(),
-    cardholder:     $('.card-holdername').val()
-  };
-
-  paymill.createToken(params, PaymillResponseHandler);
-  return false;
+    // input.setCustomValidity('');
+    // document.getElementById("payment-form").submit();
+  }
 });
