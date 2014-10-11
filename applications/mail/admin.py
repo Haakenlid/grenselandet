de40925f146753724fdabd52831a7bcbef86887e
Håken Lid <haakenlid@gmail.com>
2014-10-11 20:08:13 +0200

    hack for å sende resten av mailene

--- applications/mail/admin.py
+++ applications/mail/admin.py
@@ -12,16 +12,20 @@ class MailTriggerInline(admin.TabularInline):
     extra = 0
 
 
-
 def test_mail_trigger(modeladmin, request, queryset):
     recipient = request.user
+    try:
+        recipient = Ticket.objects.get(email=recipient.email)
+    except:
+        print('FAIL')
+        pass
     for trigger in queryset:
         trigger.send_mail(recipient)
 
 test_mail_trigger.short_description = _('send this mail to yourself.')
 
 def send_to_everyone(modeladmin, request, queryset):
-    recipients = Ticket.objects.all()
+    recipients = Ticket.objects.all()[17:]
     for trigger in queryset:
         for recipient in recipients:
             trigger.send_mail(recipient)
